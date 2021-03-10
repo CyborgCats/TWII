510cdfab7c59fe717aaf1e4906cfff97656f0b29
CyborgCats <marmirrz@gmail.com>
2021-03-09 21:36:59 -0400

    Solución de errores y finalización

--- pdf/index.php
+++ pdf/index.php
@@ -23,14 +23,14 @@ $pdf->SetFillColor(232,232,232);
 $pdf->SetFont('Arial','B',12);
 //agregamos celdas
 $pdf->Cell(35,6,utf8_decode('Nro. Préstamo'),1,0,'C',1);
-$pdf->Cell(55,6,utf8_decode('Descripción'),1,0,'C',1);
-$pdf->Cell(100,6,utf8_decode('Nombre Usuario Préstamo'),1,1,'C',1);
+$pdf->Cell(75,6,utf8_decode('Descripción'),1,0,'C',1);
+$pdf->Cell(80,6,utf8_decode('Nombre Usuario Préstamo'),1,1,'C',1);
 //colocar atributos
 while($row=$resultado->fetch_assoc())
 {
     $pdf->Cell(35,6,utf8_decode($row['PrestamoID']),1,0,'C');
-    $pdf->Cell(55,6,utf8_decode($row['Descripcion']),1,0,'C');   
-    $pdf->Cell(100,6,utf8_decode($row['NombreUsuario']),1,1,'C');
+    $pdf->Cell(75,6,utf8_decode($row['Descripcion']),1,0,'C');   
+    $pdf->Cell(80,6,utf8_decode($row['NombreUsuario']),1,1,'C');
 }
 //mostramos el pdf
 $pdf->Output();
--- php/modificarinventario.php
+++ php/modificarinventario.php
@@ -109,7 +109,7 @@
             <!-- Website Overview -->
             <div class="panel panel-default">
               <div class="panel-heading main-color-bg">
-                <h3 class="panel-title">Modificación de Préstamo</h3>
+                <h3 class="panel-title">Modificación de Inventario</h3>
               </div>
 
               <div class="panel-body">
